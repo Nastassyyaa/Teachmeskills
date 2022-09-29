31834285ec287fc7f99bb1961b3211b089ab1c14
Nastassyyaa <anastasya.gleb@yandex.by>
2022-09-29 20:13:16 +0300

    changed Another text

--- app/src/main/java/com/example/lesson4/MainActivity.java
+++ app/src/main/java/com/example/lesson4/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lesson4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("Another text");
     }
 }
